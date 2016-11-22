1a4f6bb64af25482b2e78ae333fbc9d16e180e4b
Tak1wa <abu.soft.taka@gmail.com>
2016-11-23 00:51:51 +0900

    - set appmanifest - basic implements

--- TetheringManagerSample/MainPage.xaml.cs
+++ TetheringManagerSample/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
+using Windows.Networking.NetworkOperators;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,28 @@ namespace TetheringManagerSample
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// The required device capability has not been declared in the manifest.
+        /// </remarks>
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tetheringManage = NetworkOperatorTetheringManager.CreateFromConnectionProfile(
+                                        NetworkInformation.GetInternetConnectionProfile()
+                                        );
+            
+            string AP_SSID = txtSSID.Text;
+            string AP_PASS = txtPASS.Text;
+            var accessPointConfig = new NetworkOperatorTetheringAccessPointConfiguration()
+            {
+                Ssid = AP_SSID, Passphrase = AP_PASS
+            };
+            
+            await tetheringManage.ConfigureAccessPointAsync(accessPointConfig);
         }
     }
 }
